52f4135cb67911853e7472ac6ea507237a87a6d5
austil <augustin@mrlacour.fr>
2018-08-31 19:52:51 +0100

    feat(pocket report): word_count/favorite ratio, stats and correlation

--- src/reports/pocket_report.js
+++ src/reports/pocket_report.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const sstats = require('simple-statistics');
 const {makeTable} = require('./report_utils');
 const {POCKET} = require('../pullers_const');
 const {unread, archived} = require('../../' + POCKET.FILE);
@@ -29,14 +30,14 @@ const LOWER_WPM_RATIO = (WPM - WPM_MARGIN) / WPM;
 const UPPER_WPM_RATIO = (WPM + WPM_MARGIN) / WPM;
 
 const getWordCount = a => a.word_count ? parseInt(a.word_count) : 0;
-const wordsUnread = _.sumBy(unread, getWordCount);
-const wordsFavorite = _.sumBy(archived.favorite, getWordCount);
-const wordsOthers = _.sumBy(archived.others, getWordCount);
-const wordsRedTotal = wordsFavorite + wordsOthers;
-const wordsTotal = wordsRedTotal + wordsUnread;
+const sumWordsUnread = _.sumBy(unread, getWordCount);
+const sumWordsFavorite = _.sumBy(archived.favorite, getWordCount);
+const sumWordsOthers = _.sumBy(archived.others, getWordCount);
+const sumWordsRedTotal = sumWordsFavorite + sumWordsOthers;
+const sumWordsTotal = sumWordsRedTotal + sumWordsUnread;
 
 const wordCountToTimeToRead = (wordCount) => {
-  return (wordCount / WPM / 60).toFixed(0) + 'h';
+  return (wordCount / WPM / 60).toFixed(0) + 'h'; // only in hour, need improvment
 };
 
 const timeToReadInterval = (ttr) => {
@@ -46,18 +47,46 @@ const timeToReadInterval = (ttr) => {
 const reportTimeToRead = makeTable(
   'Time to read (slow, average, fast)',
   [
-    ['Unread:',  ...timeToReadInterval(wordsUnread)],
-    ['Archived:',  ...timeToReadInterval(wordsRedTotal)],
-    ['  Favorite:',  ...timeToReadInterval(wordsFavorite)],
-    ['  Others:',  ...timeToReadInterval(wordsOthers)],
-    ['Total:',  ...timeToReadInterval(wordsTotal)],
+    ['Unread:',  ...timeToReadInterval(sumWordsUnread)],
+    ['Archived:',  ...timeToReadInterval(sumWordsRedTotal)],
+    ['  Favorite:',  ...timeToReadInterval(sumWordsFavorite)],
+    ['  Others:',  ...timeToReadInterval(sumWordsOthers)],
+    ['Total:',  ...timeToReadInterval(sumWordsTotal)],
   ]
 );
 
+// Ratio favorite / red
+const favoriteRatio = archived.favorite.length * 100 / redCountTotal;
+const favoriteTimeRatio = sumWordsFavorite * 100 / sumWordsRedTotal;
+const reportFavoriteRatio = `Favorites account for ${favoriteRatio.toFixed(2)}% of the total of 
+articles red and ${favoriteTimeRatio.toFixed(2)}% of the total reading time.`;
+
+// Stats about word count on favorite vs others (are long articles better ? is there a correlation ?)
+const wordsFavorite = _.compact(archived.favorite.map(getWordCount));
+const wordsOthers = _.compact(archived.others.map(getWordCount));
+const redStast = array => [
+  sstats.mean, sstats.standardDeviation, sstats.min, sstats.median, sstats.max
+].map(fn => fn(array));
+
+const reportRedStats = makeTable(
+  'Article stats (mean, sd, min, median, max)',
+  [
+    ['Favorite:', ...redStast(wordsFavorite) ],
+    ['Others:', ...redStast(wordsOthers) ],
+  ],
+  { formatNumber: n => n.toFixed(1), lastRowBold: false }
+);
+
+const randomVarFavorite = [...archived.favorite, ...archived.others]
+  .filter(e => getWordCount(e) > 0)
+  .map(e => e.favorite === '1' ? 1 : 0);
+const randomVarWords = [...wordsFavorite, ...wordsOthers];
+const wordFavoriteCorrelation = sstats.sampleCorrelation(randomVarWords, randomVarFavorite);
+const reportWordFavoriteCorrelation = `Correlation between word_count and favorite: ${wordFavoriteCorrelation.toFixed(3)}`;
+
 /*
 TODO
-- Ratio favorite / red
-- Stats about word count on favorite vs others (are long articles better ?)
+- Number/percentage of articles without a word_count
 - Top sources (all and favorites)
 - Most common word in title (all and favorites ?)
 - Pace of adding article vs reading article
@@ -69,4 +98,10 @@ console.log(`${reportTitle}
 ${reportCount}
 
 ${reportTimeToRead}
+
+${reportFavoriteRatio}
+
+${reportRedStats}
+
+${reportWordFavoriteCorrelation}
 `);
--- src/reports/report_utils.js
+++ src/reports/report_utils.js
@@ -3,20 +3,23 @@ const _ = require('lodash');
 
 const removeAnsiCodes = s => s.replace(/\u001b\[.*?m/g, ''); // eslint-disable-line
 
-const formatCell = (cell, index, maxSize) => {
-  return index === 0 ? _.padEnd(cell, maxSize) : _.padStart(cell, maxSize); 
+const formatCell = (value, formatNumber, index = 0, maxSize = 0) => {
+  const cellContent = _.isNumber(value) ? formatNumber(value) : value;
+  return index === 0 ? _.padEnd(cellContent, maxSize) : _.padStart(cellContent, maxSize); 
 };
 
-const makeTable = (title, data) => {
-  const columnsWidth = _.zip(...data).map(column => _.max(column.map(e => e.toString().length)));
+const makeTable = (title, data, { formatNumber = _.identity, lastRowBold = true } = {}) => {
+  const columnsWidth = _.zip(...data).map(column => _.max(column.map(e => formatCell(e, formatNumber).length)));
   const rows = data.map((row, rowIndex) => {
-    const rowString = row.map((e, columnIndex) => formatCell(e, columnIndex, columnsWidth[columnIndex])).join('  ');
-    return rowIndex === data.length - 1 ? chalk.bold(rowString) : rowString;
+    const rowString = row.map((e, columnIndex) => {
+      return formatCell(e, formatNumber, columnIndex, columnsWidth[columnIndex]);
+    }).join('  ');
+    return rowIndex === data.length - 1 && lastRowBold ? chalk.bold(rowString) : rowString;
   });
   const largestRow = _.max(rows.map(r => removeAnsiCodes(r).length));
   
   const header = chalk.bold(title);
-  const line = _.repeat('-', largestRow + 1);
+  const line = _.repeat('-', _.max([largestRow, title.length]) + 1);
   const body = rows.join('\n');
   return `${header}\n${line}\n${body}`;
 };
